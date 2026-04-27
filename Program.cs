b68330f8e9265441b7e0a53dd8bc6f6ac7cfdb8a
Roger7927 <rh523755@gmail.com>
2026-04-26 21:41:16 -0300

    fix: habilitando swagger em producao e rota raiz

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using GuardianSystem.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuração da Injeção de Dependência (Dependency Injection) para o Banco
 builder.Services.AddDbContext<GuardianContext>(options =>
     options.UseSqlite("Data Source=guardian.db"));
 
@@ -13,17 +14,22 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Lógica de Inicialização: Garante que o banco exista na primeira execução
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<GuardianContext>();
     db.Database.EnsureCreated();
 }
 
-if (app.Environment.IsDevelopment())
+// --- AJUSTE DE PRODUÇÃO (Render) ---
+// Removemos a trava do 'IsDevelopment' para o Swagger aparecer na nuvem
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sentinel Radar V1");
+    c.RoutePrefix = string.Empty; // Isso faz o Swagger ser a página inicial da URL
+});
+// -----------------------------------
 
 app.UseAuthorization();
 app.MapControllers();
